8ffd26a775fc16d0c3b359477b1b5cc6352f42ff
Kiran4753 <kirankharsel@gmail.com>
2023-05-04 12:00:57 +0530

    add delete and add function

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const todoBox = document.querySelector('.list-container');
+const addBtn = document.querySelector('.add-item');
+const inputBox = document.querySelector('.item-input');
 
+addBtn.addEventListener('click', function(event){
+    event.preventDefault()
+    addTodo(inputBox.value);
+    inputBox.value = '';
+
+})
+
+const addTodo = item =>{
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${item} <img src="./images/delete.svg" alt="" width="20px">`;
+    listItem.addEventListener('click', function(){
+        this.classList.toggle('done')
+    });
+    listItem.querySelector('img').addEventListener('click', function(){
+        listItem.remove();
+    })
+    todoBox.appendChild(listItem)
+}
